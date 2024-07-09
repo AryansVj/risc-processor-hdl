59dd349b802c086020c3033bb21d42c94fc87d54
AryansVj <aryans.viraj@gmail.com>
2024-07-09 20:17:44 +0530

    Vivado verilog project initiated

---
+++ Processor_source/Processor_source.srcs/sources_1/new/IR_decode.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/09/2024 08:16:13 PM
+// Design Name: 
+// Module Name: IR_decode
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module IR_decode(
+
+    );
+endmodule
