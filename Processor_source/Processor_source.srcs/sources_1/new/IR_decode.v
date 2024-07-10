fc37e7598a9ca1cf7421d1869b5ab4369101284c
AryansVj <aryans.viraj@gmail.com>
2024-07-10 23:01:33 +0530

    IR decoder - added the instruction keyword identification as well

--- Processor_source/Processor_source.srcs/sources_1/new/IR_decode.v
+++ Processor_source/Processor_source.srcs/sources_1/new/IR_decode.v
@@ -23,9 +23,13 @@
 module IR_decode(
     input clk, rst,
     input [31:0] IRin,
-    output p,q,u,v,w,
-    output cc, reg_d, reg_s, op, reg_t, imm, off
+    output p, q, u, v, w, cc, reg_d, reg_s, op, reg_t, imm, off,
+    output MOV, LSL, ASR, ROR, AND, ANN, IOR, XOR, ADD, SUB, MUL, DIV, LDW, STW, BR
     );
+
+    wire MOV, LSL, ASR, ROR, AND, ANN, IOR, XOR, ADD, SUB, MUL, DIV;
+    wire LDW, STW;
+    wire BR;
     
     wire p = IRin[31];
     wire q = IRin[30];
@@ -40,5 +44,26 @@ module IR_decode(
     wire reg_t = IRin[3:0];       // Target register addr
     wire imm = IRin[15:0];
     wire off = IRin[19:0];
+
+    // Register Instructions
+    assign MOV = ~p & (op==0);
+    assign LSL = ~p & (op==1);
+    assign ASR = ~p & (op==2);
+    assign ROR = ~p & (op==3);
+    assign AND = ~p & (op==4);
+    assign ANN = ~p & (op==5);
+    assign IOR = ~p & (op==6);
+    assign XOR = ~p & (op==7);
+    assign ADD = ~p & (op==8);
+    assign SUB = ~p & (op==9);
+    assign MUL = ~p & (op==10);
+    assign DIV = ~p & (op==11);
+
+    // Memory Instructions
+    assign LDW = p & ~q & ~u;
+    assign STW = p & ~q & u;
+    
+    // Branch Instructions
+    assign BR = p & q;
 
 endmodule
